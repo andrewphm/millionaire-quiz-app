8150373ec23f57275c1be3b753ef8c6a6ee8ed85
andrewphm <andrpham@gmail.com>
2021-07-09 13:26:32 -0400

    Create hooks to update the amount earned

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,41 @@
 import Trivia from './components/Trivia'
-import { useState } from "react"
+import { useEffect, useMemo, useState } from "react"
 import "./app.css"
 
 function App() {
   const [questionNumber, setQuestionNumber] = useState(1);
-  const [timeOut, setTimeOut] = useState(false);
+  const [stop, setStop] = useState(false);
+  const [earned, setEarned] = useState('$ 0');
 
   const data = [
     {
       id: 1,
-      question: 'Rolex is a compnay that specializes in what type of products?',
+      question: 'Rolex is a company that specializes in what type of products?',
       answers: [
         {
           text: "Phones",
           correct: false,
+          id: 1,
         },
         {
           text: 'Watches',
           correct: true,
+          id: 2,
         },
         {
           text: "Clothing",
           correct: false,
+          id: 3,
         },
         {
           text: "Cars",
           correct: false,
+          id: 4,
         }
       ]
     },
     {
-      id: 1,
+      id: 2,
       question: 'In what country is the famous car brand Honda from?',
       answers: [
         {
@@ -53,23 +58,26 @@ function App() {
     }
   ]
 
-  const moneyPyramid = [
-    { id: 1, amount: "$ 100" },
-    { id: 2, amount: "$ 200" },
-    { id: 3, amount: "$ 300" },
-    { id: 4, amount: "$ 500" },
-    { id: 5, amount: "$ 1,000" },
-    { id: 6, amount: "$ 2,000" },
-    { id: 7, amount: "$ 4,000" },
-    { id: 8, amount: "$ 8,000" },
-    { id: 9, amount: "$ 16,000" },
-    { id: 10, amount: "$ 32,000" },
-    { id: 11, amount: "$ 64,000" },
-    { id: 12, amount: "$ 125,000" },
-    { id: 13, amount: "$ 250,000" },
-    { id: 14, amount: "$ 500,000" },
-    { id: 15, amount: "$ 1,000,000" },
-  ]
+  const moneyPyramid = useMemo(() => 
+    [
+      { id: 1, amount: "$ 100" },
+      { id: 2, amount: "$ 200" },
+      { id: 3, amount: "$ 300" },
+      { id: 4, amount: "$ 500" },
+      { id: 5, amount: "$ 1,000" },
+      { id: 6, amount: "$ 2,000" },
+      { id: 7, amount: "$ 4,000" },
+      { id: 8, amount: "$ 8,000" },
+      { id: 9, amount: "$ 16,000" },
+      { id: 10, amount: "$ 32,000" },
+      { id: 11, amount: "$ 64,000" },
+      { id: 12, amount: "$ 125,000" },
+      { id: 13, amount: "$ 250,000" },
+      { id: 14, amount: "$ 500,000" },
+      { id: 15, amount: "$ 1,000,000" },
+    ].reverse(),
+  [])
+
   const moneyList = moneyPyramid.map(obj => {
     return (
       <li className={questionNumber === obj.id ? "moneyListItem active" : "moneyListItem"}>
@@ -79,29 +87,40 @@ function App() {
     )
   })
 
-  console.log(moneyList)
+  //Hook to change the amount earned
+  useEffect(()=> {
+    if(questionNumber > 1) {
+      setEarned(moneyPyramid.find((element) => element.id === questionNumber - 1).amount);
+    }
+  },[moneyPyramid, questionNumber])
+
 
   return (
     <div className="app">
       <div className="main">
+        {stop ? (
+          <h1 className="endText">You earned: {earned}</h1>
+        ) : (
+         <> 
+          <div className="main__top">
+            <div className="timer">30</div>
+          </div>
 
-        <div className="main__top">
-          <div className="timer">30</div>
-        </div>
-
-        <div className="main__bottom">
-          <Trivia 
-          data={data} 
-          setTimeOut={setTimeOut}
-          questionNumber={questionNumber} 
-          setQuestionNumber={setQuestionNumber}
-          />
-        </div>
+          <div className="main__bottom">
+           <Trivia 
+             data={data} 
+             setStop={setStop}
+             questionNumber={questionNumber} 
+             setQuestionNumber={setQuestionNumber}
+           />
+          </div>
+         </> 
+        )}
       </div>
 
       <div className="pyramid">
         <ul className="moneyList">
-          {moneyList.reverse()}
+          {moneyList}
         </ul>
       </div>
 
--- src/components/Trivia.jsx
+++ src/components/Trivia.jsx
@@ -3,24 +3,56 @@ import "../app.css"
 
 const Trivia = ({
     data, 
-    setTimeOut, 
+    setStop, 
     questionNumber, 
     setQuestionNumber,
 }) => {
     const [question, setQuestion] = useState(null);
+    const [selectedAnswer, setSelectedAnswer] = useState(null);
+    const [className, setClassName] = useState('answer');
 
     useEffect(() => {
+        console.log('useEffect on mount')
         setQuestion(data[questionNumber - 1])
     },[data, questionNumber])
 
+    const delay = (duration, callback) => {
+        setTimeout(()=>{
+            callback()
+        }, duration)
+    }
+
+    const handleClick = (arr) => {
+        setSelectedAnswer(arr);
+        setClassName('answer active')
+        delay(1000, () => setClassName(arr.correct ? "answer correct" : "answer incorrect") 
+        )
+        delay(6000, () => {
+            if(arr.correct) {
+                setQuestionNumber((prev) => prev + 1);
+                setSelectedAnswer(null)
+            } else {
+                setStop(true);
+            }
+        })
+
+    }
+
     return (
         <div className="trivia">
+
             <div className="question">{question?.question}</div>
+
             <div className="answers">
                 {question?.answers.map(arr => (
-                    <div className="answer">{arr.text}</div>
+                    <div 
+                    className={selectedAnswer === arr ? className : "answer"} 
+                    onClick={() => handleClick(arr)}
+                    key={arr.id}
+                    >{arr.text}</div>
                 ))}
             </div>
+
         </div>
     )
 }
